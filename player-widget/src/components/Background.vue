f8ac0088ba4349c9a4fe0125baa075999e5e6c2b
Martyna Michno <mjjmichno@gmail.com>
2020-02-04 12:58:44 +0100

    background for main view with girls added

--- player-widget/src/components/Background.vue
+++ player-widget/src/components/Background.vue
@@ -15,7 +15,7 @@ export default {
 .background {
     position: fixed;
     top: 0;
-    z-index: -1;
+    z-index: -10;
     width: 100vw;
     height: 100vh;
     background-image: url('../assets/background.png');
--- player-widget/src/components/Main.vue
+++ player-widget/src/components/Main.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="main">
+      <div class="music-panel">
+          <div class="layer">
+
+          </div>
+      </div>
   </div>
 </template>
 
@@ -16,5 +21,26 @@ name: 'Main'
     border-radius: 29px;
     background: white;
 }
+
+.music-panel{
+    position: relative;
+    border-radius: 27px 27px 0 0;
+    height: 337px;
+    width: 360px;
+    background-image: url('../assets/girls.jpg');
+    background-size: cover;
+}
+
+.layer {
+    border-radius: 27px 27px 0 0;
+    background-color: #544282;
+    opacity: 0.3;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
 
 </style>
