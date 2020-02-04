ef6ad797dcf632d20fff77a19924612f8e588fc8
Martyna Michno <mjjmichno@gmail.com>
2020-02-04 17:25:23 +0100

    added data - list of songs

---
+++ player-widget/src/data.js
@@ -0,0 +1,53 @@
+export default [
+    {
+        artist: 'Taylor Swift',
+        name: 'Lover',
+        time: '3:45'
+    },
+    {
+        artist: 'Taylor Swift',
+        name: 'Picture to Burn',
+        time: '3:52'
+    },
+    {
+        artist: 'Taylor Swift',
+        name: 'Teardrops on My Guitar',
+        time: '4:25'
+    },
+    {
+        artist: 'Taylor Swift',
+        name: 'Cold as You',
+        time: '2:59'
+    },
+    {
+        artist: 'Taylor Swift',
+        name: 'Stay Beautiful',
+        time: '2:56'
+    },
+    {
+        artist: 'Taylor Swift',
+        name: 'Our Song',
+        time: '4:15'
+    },
+    {
+        artist: 'Taylor Swift',
+        name: 'The Outside',
+        time: '4:18'
+    },
+    {
+        artist: 'Taylor Swift',
+        name: 'Hello',
+        time: '2:35'
+    },
+    {
+        artist: 'Taylor Swift',
+        name: 'Girlfriend',
+        time: '2:17'
+    },
+    {
+        artist: 'Taylor Swift',
+        name: 'My Life',
+        time: '3:05'
+    },
+
+]
